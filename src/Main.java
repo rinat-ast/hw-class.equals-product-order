4df806adb004f7257fd28022485be57d359b0c9c
rinat-ast <rinat-90210@mail.ru>
2026-01-02 19:01:12 +0400

    initial hw-class.equals-product-order-for-check

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Product uncleBob =new Product(13,"Тушёнка Uncle Bob",400,"Консервы");
+        Product uncleBob2 =new Product(13,"Тушёнка Uncle Bob",400,"Консервы");
+        Product secondFront =new Product(14,"Тушёнка Второй фронт",400,"Консервы");
+        Product doshirak = new Product(200,"Доширак",58,"Бич пакет");
+        Product mayonnaise = new Product(201,"Мистер Рико",120,"Майонез");
+        Product toiletPaper = new Product(300,"Zewa",150,"Тулетная бумага");
 
+        System.out.println(uncleBob2.equals(uncleBob));
+
+        Product [] alexOrder = {uncleBob,secondFront,doshirak,mayonnaise,toiletPaper};
+        Product [] jimOrder = {secondFront,doshirak,mayonnaise,toiletPaper,uncleBob2};
+
+        Order alex = new Order("Alex",alexOrder);
+        Order jim = new Order("Jim",jimOrder);
+
+        System.out.println(Arrays.toString(alex.basket));
+        System.out.println(Arrays.toString(alexOrder));
+        System.out.println(alex);
+
+        System.out.println(jim.equals(alex));
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,35 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Order {
+    String customer;
+    Product[] basket;
+
+    public Order(String customer, Product[] basket) {
+        this.customer = customer;
+        this.basket = basket;
+
+
+    }
+
+    @Override
+    public String toString(){//чисто для эксперимента
+        String [] orderName = new String[basket.length];
+        int [] orderPrice = new int[basket.length];
+        for (int i = 0; i < basket.length; i++) {
+            orderName[i] = basket[i].name;
+            orderPrice[i] = basket[i].price;
+        }
+        return "Заказ " + customer + ". Наименование товаров: " + Arrays.toString(orderName)
+                + " Цена товаров: " + Arrays.toString(orderPrice);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Order order = (Order) obj;
+        return Objects.equals(customer, order.customer) && Arrays.equals(basket, order.basket);
+    }
 }
