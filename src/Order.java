1a5a756f2b0cf435f8319991fd3909911b7258d1
rinat-ast <rinat-90210@mail.ru>
2026-01-07 16:48:16 +0400

    initial hw-class.equals-product-order-for-check-4

--- src/Order.java
+++ src/Order.java
@@ -25,16 +25,17 @@ public class Order {
     @Override
     public boolean equals(Object o) {
         Order order = (Order) o;
-        if (customer != order.customer) return false;
+        if (customer.equals(order.customer)) return false;
+        if (basket == null) return false;
+        if (order.basket == null) return false;
         if (basket.length != order.basket.length) return false;
         for (int i = 0; i < basket.length; i++) {
+            if (basket[i].name == null) return false;
+            if (order.basket[i].name == null) return false;
             if (!basket[i].name.equals(order.basket[i].name)){
 //                System.out.println(basket[i].name +" -и- "+ order.basket[i].name);
                 return false;
             }
-            if (basket[i].name == null) return false;
-            if (order.basket[i].name == null) return false;
-
         }
         return true;
 
